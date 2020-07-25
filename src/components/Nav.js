d1dff3799dd0edbbb9e983538ff4e9a8192d71df
rafael <rafquadros22@gmail.com>
2020-07-25 01:19:04 -0400

    middle commite

---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from "react";
+import SearchName from "./SearchName.js";
+import SearchDOB from "./SearchDOB.js";
+import "../styles/Nav.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse row" id="navbarNav">
+        {/* <div className="input-group col-8">
+          <SearchDOB />
+        </div> */}
+        <div className="search-area col-4">
+          <SearchName />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
